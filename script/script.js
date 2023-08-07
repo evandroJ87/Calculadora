457d2f50ee9dd73026a31672cec0108e07fe6e50
Evandro <evandrosilva8744@gmail.com>
2023-08-07 01:31:13 -0300

    att

--- script/script.js
+++ script/script.js
@@ -14,17 +14,69 @@ class Calculator {
             return;
         }
         this.currentOperation=digit
-        this.updateScreen()
+        this.updateScreen();
     }
     // Calculator
 
     processOperation(operation){
-        console.log(operation)
+        //Check if current is empty
+        if(this.currentOperation.innerText===""){
+            if(this.previousOperationText.innerText !== ""){
+                this.changeOperation(operation);
+            }
+            return
+        }
+
+
+        //Get current and previous value
+        let operationValue
+        const previous = +this.previousOperationText.innerText.split(" ")[0];
+        const current = +this.currentOperationText.innerText;
+
+        switch(operation){
+            case "+":
+                operationValue = previous + current
+                this.updateScreen(operationValue,operation, current, previous)
+                break;
+            case "-":
+                operationValue = previous - current
+                this.updateScreen(operationValue,operation, current, previous)
+                break;
+            case "/":
+                operationValue = previous / current
+                this.updateScreen(operationValue,operation, current, previous)
+                break;
+            case "*":
+                operationValue = previous * current
+                this.updateScreen(operationValue,operation, current, previous)
+                break;
+            default:
+            return;
+        }
     }
 
     //change values of the calculator screen
-    updateScreen() {
-        this.currentOperationText.innerText += this.currentOperation;
+    updateScreen(operationValue=null, operation = null, current=null, previous=null) {
+        if (operationValue===null){
+            this.currentOperationText.innerText += this.currentOperation;
+        } else{
+            if (previous===0){
+                operationValue = current
+            }
+            this.previousOperationText.innerText = `${operationValue} ${operation}`
+            this.currentOperationText.innerText = ''
+        }
+    }
+// FALTA MUDAR PRA MUDAR PARA AS ATUALIZAÇÕES DE OPERAÇOES **testar colocar as 4 operaçoes**
+    //Change math operation
+    changeOperation(operation){
+        const mathOperations = ["*", "+", "/", "-"];
+        
+        if(!mathOperations.includes(operation)){
+            return;
+        }
+
+        this.previousOperationText.innerText = this.previousOperationText.innerText.slice(0, -1) + operation;
     }
 }
 
